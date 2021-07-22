6b04e094c8403fcf21608d5d3f5ee0b7675ed386
Jiri Grill <jiri.grill@workday.com>
2021-07-22 15:08:32 +0200

    after 250. Breather and Review

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+
+
+const API_KEY = 'AIzaSyD4LRhJ8TA30Y9WKFKZEXPGuuAlH5oy_wA';
+
 // Create a new component. This component should produce
 // some html
 
 const App  = () => {
-  return <div>Hi!</div>;
-}
+  return (
+    <div>
+    <SearchBar />
+    </div>
+  );
+};
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
