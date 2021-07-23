77d33ad8c7cf9b6c8812883aa906e1c4981c93c0
Jiri Grill <jiri.grill@workday.com>
2021-07-23 09:36:36 +0200

    Finish lecture 256 - video_list & video_list_item

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyD4LRhJ8TA30Y9WKFKZEXPGuuAlH5oy_wA';
+
+
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 
 // Create a new component. This component should produce
 // some html
 
-const App  = () => {
-  return (
-    <div>
-    <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 };
 
 // Take this component's generated HTML and put it
